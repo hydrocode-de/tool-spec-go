eb76c962a95f0347d8b097e247d85a5d1eab9ff2
Mirko Mälicke <mirko@hydrocode.de>
2025-11-24 08:43:20 +0100

    add a validation module

---
+++ validate/validate.go
@@ -0,0 +1,57 @@
+package validate
+
+import (
+	"fmt"
+
+	toolspec "github.com/hydrocode-de/tool-spec-go"
+)
+
+type AllowedField string
+
+const (
+	Parameters AllowedField = "parameters"
+	Data       AllowedField = "data"
+)
+
+type ErrorType string
+
+const (
+	WrongType       ErrorType = "wrong-type"
+	NotArray        ErrorType = "not-array"
+	OutOfRange      ErrorType = "out-of-range"
+	NotInEnum       ErrorType = "not-in-enum"
+	InvalidDateTime ErrorType = "invalid-datetime"
+	Required        ErrorType = "required"
+	NotAllowed      ErrorType = "not-allowed"
+)
+
+type ValidationError struct {
+	Field    AllowedField `json:"field"`
+	Name     string       `json:"name"`
+	Type     ErrorType    `json:"type"`
+	Expected string       `json:"expected"`
+	Actual   string       `json:"actual"`
+	Message  string       `json:"message"`
+}
+
+func (e *ValidationError) Error() string {
+	return fmt.Sprintf("%s: %s (expected %s, got %s)", e.Name, e.Message, e.Expected, e.Actual)
+}
+
+func ValidateInputs(spec toolspec.ToolSpec, inputs toolspec.ToolInput) (bool, []error) {
+	var errors []error = make([]error, 0)
+
+	didError, errs := ValidateParameters(spec, inputs.Parameters, false)
+	if didError {
+		errors = append(errors, errs...)
+	}
+	didError, errs = ValidateData(spec, inputs.Datasets)
+	if didError {
+		errors = append(errors, errs...)
+	}
+
+	if len(errors) > 0 {
+		return true, errors
+	}
+	return false, nil
+}
